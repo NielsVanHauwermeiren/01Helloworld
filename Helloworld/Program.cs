c25fd06b04b4c38e401f4724b4f8c793190f4a9f
Niels Van Hauwermeiren <niels.vanhauwermeiren@campus.romerocollege.be>
2019-09-26 10:36:44 +0200

    Add project files.

---
+++ Helloworld/Program.cs
@@ -0,0 +1,25 @@
+﻿ // 5AIT3 - 2019-09-11
+ /* v1: toon "hello world" op het scherm
+  */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Toon "hello world" op het scherm
+            Console.WriteLine("Hello world!");
+
+            // wachten...
+            Console.Write("druk op Enter om af te sluiten...");
+            Console.ReadLine();
+            
+        }
+    }
+}
